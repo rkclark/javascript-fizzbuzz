d9139e21ade6fdc208114953223b6eb6085630a5
Mike Field-May <mfieldmay@gmail.com>
2017-01-31 16:56:58 +0000

    Added test for 10

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,37 +1,36 @@
 describe("FizzBuzz", function() {
 
-  // var FizzBuzz;
-  // var fb = new FizzBuzz();
-  // console.log(fb);
-  //
-  // beforeEach(function() {
-  //   FizzBuzz = new FizzBuzz();
-  //   console.log(FizzBuzz);
-  // });
+   var fb;
+
+
+  beforeEach(function() {
+    fb = new FizzBuzz();
+  });
 
   describe("#print", function() {
 
     it("Responds to #print", function() {
-      var fb = new FizzBuzz();
+      // var fb = new FizzBuzz();
       expect(fb.print).toBeDefined();
     });
 
     it("Returns a number when not divisible by 3 or 5", function() {
-      var fb = new FizzBuzz();
+      // var fb = new FizzBuzz();
       expect(fb.print(4)).toEqual(4);
       expect(fb.print(2)).toEqual(2);
     });
 
     it("Returns fizz when divisible by 3", function(){
-      var fb = new FizzBuzz();
+      // var fb = new FizzBuzz();
       expect(fb.print(3)).toEqual("Fizz");
       expect(fb.print(6)).toEqual("Fizz");
 
     });
 
     it("Returns buzz when divisible by 5", function() {
-      var fb = new FizzBuzz();
+      // var fb = new FizzBuzz();
       expect(fb.print(5)).toEqual("Buzz");
+      expect(fb.print(10)).toEqual("Buzz");
 
     });
 
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -8,6 +8,11 @@ FizzBuzz.prototype.print = function(number) {
   if ( number % 3 === 0 ) {
     return 'Fizz';
   }
+
+  if ( number === 5 ) {
+      return 'Buzz';
+  };
+
   return number;
 
 };
