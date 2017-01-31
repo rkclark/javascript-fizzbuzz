ca745a74bd88ed9206564f732cb59063fb2baf67
Rick Clark <rkingsleyclark@gmail.com>
2017-01-31 17:10:46 +0000

    refactor fizzbuzz

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -5,18 +5,31 @@ function FizzBuzz() {
 
 FizzBuzz.prototype.print = function(number) {
 
-  if ( number % 15 === 0) {
+  // if ( number % 15 === 0) {
+  //   return 'FizzBuzz';
+  // } else {
+  //   if ( number % 3 === 0 ) {
+  //     return 'Fizz';
+  //   } else {
+  //     if ( number % 5 === 0 ) {
+  //         return 'Buzz';
+  //     } else {
+  //       return number;
+  //     };
+  //   }
+  // };
+
+  if ( this._divisibleByFifteen(number) ) {
     return 'FizzBuzz';
-  };
+  } else if ( number % 3 === 0 ) {
+      return 'Fizz';
+    } else if ( number % 5 === 0 ) {
+          return 'Buzz';
+      } else {
+        return number;
+      };
+};
 
-  if ( number % 3 === 0 ) {
-    return 'Fizz';
-  }
-
-  if ( number % 5 === 0 ) {
-      return 'Buzz';
-  };
-
-  return number;
-
+FizzBuzz.prototype._divisibleByFifteen = function(number) {
+  return number % 15 === 0
 };
