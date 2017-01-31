7ba06363baf037af0d73f2e2ceebfb43578ebca1
Mike Field-May <mfieldmay@gmail.com>
2017-01-31 17:42:01 +0000

    Created #go and further refactoring

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -5,29 +5,23 @@ function FizzBuzz() {
 
 FizzBuzz.prototype.print = function(number) {
 
-  if ( this._divisibleByFifteen(number) ) {
+  if ( this._divisibleBy(number, 15) ) {
     return 'FizzBuzz';
-  } else if ( this._divisibleByThree(number) ) {
+  } else if ( this._divisibleBy(number, 3) ) {
       return 'Fizz';
-  } else if ( this._divisibleByFive(number) ) {
+  } else if ( this._divisibleBy(number, 5) ) {
       return 'Buzz';
   } else {
       return number;
   };
 };
 
-FizzBuzz.prototype._divisibleByFifteen = function(number) {
-  return number % 15 === 0
-};
-
-FizzBuzz.prototype._divisibleByFive = function(number) {
-  return number % 5 === 0
-};
-
-FizzBuzz.prototype._divisibleByThree = function(number) {
-    return number % 3 === 0
+FizzBuzz.prototype._divisibleBy = function(number, divisor) {
+  return number % divisor === 0
 };
 
 FizzBuzz.prototype.go = function () {
-  return true;
+  for ( var i = 1; i <= 100; i += 1 ) {
+    console.log(this.print(i));
+  };
 };
