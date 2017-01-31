6bccd561c3f40e8a750d69ec85deec698d73ff67
Mike Field-May <mfieldmay@gmail.com>
2017-01-31 17:14:58 +0000

    Refactored logic out of the .print method

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -5,31 +5,25 @@ function FizzBuzz() {
 
 FizzBuzz.prototype.print = function(number) {
 
-  // if ( number % 15 === 0) {
-  //   return 'FizzBuzz';
-  // } else {
-  //   if ( number % 3 === 0 ) {
-  //     return 'Fizz';
-  //   } else {
-  //     if ( number % 5 === 0 ) {
-  //         return 'Buzz';
-  //     } else {
-  //       return number;
-  //     };
-  //   }
-  // };
-
   if ( this._divisibleByFifteen(number) ) {
     return 'FizzBuzz';
-  } else if ( number % 3 === 0 ) {
+  } else if ( this._divisibleByThree(number) ) {
       return 'Fizz';
-    } else if ( number % 5 === 0 ) {
-          return 'Buzz';
-      } else {
-        return number;
-      };
+  } else if ( this._divisibleByFive(number) ) {
+      return 'Buzz';
+  } else {
+      return number;
+  };
 };
 
 FizzBuzz.prototype._divisibleByFifteen = function(number) {
   return number % 15 === 0
+};
+
+FizzBuzz.prototype._divisibleByFive = function(number) {
+  return number % 5 === 0
+};
+
+FizzBuzz.prototype._divisibleByThree = function(number) {
+    return number % 3 === 0
 };
